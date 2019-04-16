26aa8d91ae6880d93b60de08a8d6ef15a20a209c
sylko.olzscher <CmdPirx@gmail.com>
2019-04-16 16:48:50 +0200

    SQLite update 3.27.2

--- 3party/shell.cmake
+++ 3party/shell.cmake
@@ -3,13 +3,13 @@ set (shell)
 
 set (shell_cpp
 
-    3party/sqlite-amalgamation-3250000/shell.c  
+    3party/sqlite-amalgamation-3270200/shell.c  
 
 )
 
 set (shell_h
 
-    3party/sqlite-amalgamation-3250000/sqlite3.h
+    3party/sqlite-amalgamation-3270200/sqlite3.h
    
 )
 
