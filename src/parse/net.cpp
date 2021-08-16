81acf2b19110c8b52f1301769a463f62aec15106
Sylko Olzscher <sylko.olzscher@solostec.ch>
2021-08-16 10:54:02 +0200

    IPv6 address construction fixed

--- src/parse/net.cpp
+++ src/parse/net.cpp
@@ -7,6 +7,8 @@
 #include <cyng/parse/net.h>
 #include <cyng/parse/buffer.h>
 
+#include <iostream>
+
 #include <boost/assert.hpp>
 #include <boost/asio.hpp>
 
@@ -26,8 +28,26 @@ namespace cyng {
 			return boost::asio::ip::make_address_v6(bytes, scope_id);
 		}
 		//std::cout << boost::asio::ip::make_address_v6(t, 0) << std::endl;
-		return boost::asio::ip::address();
+		return boost::asio::ip::address_v6();
 	}
+
+#if defined(BOOST_OS_LINUX_AVAILABLE)
+	boost::asio::ip::address to_ipv6(std::string hex_str, std::string device) {
+
+		boost::system::error_code ec;
+		//	fe80000000000000f45607fffe46e821
+		//	fe80::f456:7ff:fe46:e821
+		//boost::asio::detail::array<unsigned char, 16>
+		auto const tmp = to_ipv6(hex_str, 0).to_string();
+		auto const str = tmp + "%" + device;
+		// std::cerr << std::endl << "***\t" << str << std::endl;
+		auto const addr = boost::asio::ip::make_address_v6(str, ec);
+		return (!ec)
+			? addr
+			: boost::asio::ip::address_v6()
+			;
+	}
+#endif
 
 }
 
