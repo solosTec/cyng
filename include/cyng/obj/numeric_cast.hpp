fe11bf90b17b5c77a0a76494326511335a69e074
Sylko Olzscher <sylko.olzscher@gmail.com>
2022-01-12 14:24:13 +0100

    problem with numeric boolean cast fixed

--- include/cyng/obj/numeric_cast.hpp
+++ include/cyng/obj/numeric_cast.hpp
@@ -50,25 +50,85 @@ namespace cyng {
 		static_assert(std::is_arithmetic<T>::value, "only arithmetic types supported");
 		try {
 			switch (obj.rtti().tag()) {
-			case TC_BOOL:
-				return value_cast(obj, false)
+			case TC_BOOL: {
+				auto const b = value_cast(obj, false);
+				//	if expected and provided type both booleans don't return a numeric limit
+				if constexpr (std::is_same_v<T, bool>) {
+					return b;
+				}
+				return b
 					? std::numeric_limits<T>::lowest()
 					: std::numeric_limits<T>::max()
 					;
+			}
 
 			case TC_CHAR:	return boost::numeric_cast<T>(value_cast<char>(obj, 0));
 			case TC_UINT8:	return boost::numeric_cast<T>(value_cast<std::uint8_t>(obj, 0));
-			case TC_UINT16:	return boost::numeric_cast<T>(value_cast<std::uint16_t>(obj, 0));
-			case TC_UINT32:	return boost::numeric_cast<T>(value_cast<std::uint32_t>(obj, 0));
-			case TC_UINT64:	return boost::numeric_cast<T>(value_cast<std::uint64_t>(obj, 0));
+			case TC_UINT16: {
+				auto const n = value_cast<std::uint16_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::uint16_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
+			case TC_UINT32: {
+				auto const n = value_cast<std::uint32_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::uint32_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
+			case TC_UINT64: {
+				auto const n = value_cast<std::uint64_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::uint64_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
 			case TC_INT8:	return boost::numeric_cast<T>(value_cast<std::int8_t>(obj, 0));
-			case TC_INT16:	return boost::numeric_cast<T>(value_cast<std::int16_t>(obj, 0));
-			case TC_INT32:	return boost::numeric_cast<T>(value_cast<std::int32_t>(obj, 0));
-			case TC_INT64:	return boost::numeric_cast<T>(value_cast<std::int64_t>(obj, 0));
+			case TC_INT16: {
+				auto const n = value_cast<std::int16_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::int16_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
+			case TC_INT32: {
+				auto const n = value_cast<std::int32_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::int32_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
+			case TC_INT64:	{
+				auto const n = value_cast<std::int64_t>(obj, 0);
+				if constexpr (std::is_same_v<T, std::int64_t>) {
+					return n;
+				}
+				return boost::numeric_cast<T>(n);
+			}
 
-			case TC_FLOAT:		return static_cast<T>(value_cast<float>(obj, 0.0));
-			case TC_DOUBLE:		return static_cast<T>(value_cast<double>(obj, 0.0));
-			case TC_FLOAT80:	return static_cast<T>(value_cast<long double>(obj, 0.0));
+			case TC_FLOAT: {
+				auto const f = value_cast<float>(obj, 0.0);
+				if constexpr (std::is_same_v<T, float>) {
+					return f;
+				}
+				return static_cast<T>(f);
+			}
+			case TC_DOUBLE: {
+				auto const d = value_cast<double>(obj, 0.0);
+				if constexpr (std::is_same_v<T, double>) {
+					return d;
+				}
+				return static_cast<T>(d);
+			}
+			case TC_FLOAT80: {
+				auto const d = value_cast<long double>(obj, 0.0);
+				if constexpr (std::is_same_v<T, long double>) {
+					return d;
+				}
+				return static_cast<T>(d);
+			}
 			case TC_STRING:		return string_to_numeric<T>(obj, def);
 			default:
 				break;
