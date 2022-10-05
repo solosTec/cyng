b61b1e50e951ea0aad4a565ae596f8eb3a891546
Sylko Olzscher <sylko.olzscher@gmail.com>
2022-10-05 08:48:57 +0200

    fix: assignment operator closes existing connection first

--- include/cyng/net/client_proxy.h
+++ include/cyng/net/client_proxy.h
@@ -7,43 +7,43 @@
 #ifndef CYNG_NET_CLIENT_PROXY_H
 #define CYNG_NET_CLIENT_PROXY_H
 
-#include <cyng/task/channel.h>
 #include <cyng/obj/intrinsics/buffer.h>
+#include <cyng/task/channel.h>
 
 namespace cyng {
-	namespace net {
-		class client_proxy
-		{
-		public:
-			client_proxy();
-			client_proxy(channel_ptr);
-			client_proxy(client_proxy const&) = default;
+    namespace net {
+        class client_proxy {
+          public:
+            client_proxy();
+            client_proxy(channel_ptr);
+            client_proxy(client_proxy const &) = default;
 
-			/**
-			 * Assign a (new) channel
-			 */
-			client_proxy& operator=(channel_ptr);
+            /**
+             * Assign a (new) channel
+             */
+            client_proxy &operator=(channel_ptr);
+            client_proxy &operator=(client_proxy &&);
 
-			/**
-			 * close client
-			 */
-			void stop();
+            /**
+             * close client
+             */
+            void stop();
 
-			/**
-			 * open connection
-			 */
-			void connect(std::string host, std::string service);
+            /**
+             * open connection
+             */
+            void connect(std::string host, std::string service);
 
-			/**
-			 * send 
-			 */
-			void send(cyng::buffer_t&& data);
-			void send(std::string const& data);
+            /**
+             * send
+             */
+            void send(cyng::buffer_t &&data);
+            void send(std::string const &data);
 
-		private:
-			channel_ptr	client_;
-		};
-	}
-}
+          private:
+            channel_ptr client_;
+        };
+    } // namespace net
+} // namespace cyng
 
 #endif //	CYNG_NET_CLIENT_HPP
--- include/cyng/net/server_proxy.h
+++ include/cyng/net/server_proxy.h
@@ -10,33 +10,33 @@
 #include <cyng/task/channel.h>
 
 namespace cyng {
-	namespace net {
-		class server_proxy
-		{
-		public:
-			server_proxy();
-			server_proxy(channel_ptr);
-			server_proxy(server_proxy const&) = default;
+    namespace net {
+        class server_proxy {
+          public:
+            server_proxy();
+            server_proxy(channel_ptr);
+            server_proxy(server_proxy const &) = default;
 
-			/**
-			 * Assign a (new) channel
-			 */
-			server_proxy& operator=(channel_ptr);
+            /**
+             * Assign a (new) channel
+             */
+            server_proxy &operator=(channel_ptr);
+            server_proxy &operator=(server_proxy &&);
 
-			/**
-			 * close client
-			 */
-			void stop();
+            /**
+             * close client
+             */
+            void stop();
 
-			/**
-			 * start listening
-			 */
-			void start();
+            /**
+             * start listening
+             */
+            void start();
 
-		private:
-			channel_ptr	server_;
-		};
-	}
-}
+          private:
+            channel_ptr server_;
+        };
+    } // namespace net
+} // namespace cyng
 
 #endif
