29cc290a4990c28ad3984952deed47f4fc1082fd
Sylko Olzscher <sylko.olzscher@gmail.com>
2022-11-01 19:44:33 +0100

    style: include ordering

--- include/cyng/io/io.h
+++ include/cyng/io/io.h
@@ -7,91 +7,87 @@
 #ifndef CYNG_IO_H
 #define CYNG_IO_H
 
-#include <ostream>
 #include <cyng/io/ostream.h>
 
+#include <ostream>
+
 namespace cyng {
-	namespace io {
+    namespace io {
 
-		//
-		//	serialization tags
-		//
+        //
+        //	serialization tags
+        //
 
-		/**
-		 * Simple serialization to human readable text
-		 */
-		struct PLAIN {};
+        /**
+         * Simple serialization to human readable text
+         */
+        struct PLAIN {};
 
-		/**
-		 * Serialization to readable output
-		 * with type information
-		 */
-		struct TYPED {};
+        /**
+         * Serialization to readable output
+         * with type information
+         */
+        struct TYPED {};
 
-		/**
-		 * Serialization to a readable hierarchical, tree-like structure
-		 */
-		struct PRETTY {};
+        /**
+         * Serialization to a readable hierarchical, tree-like structure
+         */
+        struct PRETTY {};
 
-		/**
-		 * Serialization to an internal binary
-		 * format with TLV (type-length-value) encoding.
-		 */
-		struct BINARY {};
+        /**
+         * Serialization to an internal binary
+         * format with TLV (type-length-value) encoding.
+         */
+        struct BINARY {};
 
-		/**
-		 * Serialization to JSON.
-		 */
-		struct JSON {};
-		//struct JSON_PRETTY {};
+        /**
+         * Serialization to JSON.
+         */
+        struct JSON {};
 
-		/**
-		 * Serialization to XML.
-		 */
-		struct XML {};
+        /**
+         * Serialization to XML.
+         */
+        struct XML {};
 
-		/**
-		 * Serialization to CSV.
-		 */
-		struct CSV {};
+        /**
+         * Serialization to CSV.
+         */
+        struct CSV {};
 
-		/**
-		 * Serialization to C++.
-		 * Cenerate C++ source code compatible with this library.
-		 */
-		struct CPP {};
+        /**
+         * Serialization to C++.
+         * Cenerate C++ source code compatible with this library.
+         */
+        struct CPP {};
 
-		/**
-		 * RAAI class to get written byte count
-		 */
-		class calc_size
-		{
-		public:
-			calc_size(std::ostream& os);
-			operator std::size_t() const;
+        /**
+         * RAAI class to get written byte count
+         */
+        class calc_size {
+          public:
+            calc_size(std::ostream &os);
+            operator std::size_t() const;
 
-		private:
-			std::size_t count() const;
+          private:
+            std::size_t count() const;
 
-		private:
-			std::streampos const pos_;
-			std::ostream& os_;
-		};
+          private:
+            std::streampos const pos_;
+            std::ostream &os_;
+        };
 
-		/**
-		 * generic serializer
-		 */
-		template <typename T, typename TAG>
-		struct serializer
-		{
-			static std::size_t write(std::ostream& os, T const& v)
-			{
-				calc_size const cs(os);
-				os << v;
-				return cs;
-			}
-		};
+        /**
+         * generic serializer
+         */
+        template <typename T, typename TAG> struct serializer {
+            static std::size_t write(std::ostream &os, T const &v) {
+                calc_size const cs(os);
+                os << v;
+                return cs;
+            }
+        };
 
-	}
-}
+    } // namespace io
+} // namespace cyng
 #endif
