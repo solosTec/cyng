aeba00253ead79056bce916d5c9ea9c26c1204ff
Sylko Olzscher <sylko.olzscher@gmail.com>
2023-03-27 09:41:23 +0200

    doc: versioning scheme

--- include/cyng/obj/intrinsics/version.h
+++ include/cyng/obj/intrinsics/version.h
@@ -5,10 +5,10 @@
  *
  */
 #ifndef CYNG_OBJ_INTRINSCIS_VERSION_H
-#define CYNG_OBJ_INTRINSCIS_VERSION_H	
+#define CYNG_OBJ_INTRINSCIS_VERSION_H
 
-#include <cstdint>
 #include <boost/predef.h>
+#include <cstdint>
 #if defined(BOOST_OS_LINUX)
 #include <sys/types.h>
 #undef major
@@ -18,125 +18,103 @@
 
 namespace cyng {
 
-	namespace details	{
+    namespace details {
 
-		template <typename T, typename U>
-		struct part {
-			static_assert(sizeof(T) * 2 == sizeof(U), "wrong datatype size");
+        template <typename T, typename U> struct part {
+            static_assert(sizeof(T) * 2 == sizeof(U), "wrong datatype size");
 
-#pragma pack(push,1)
+#pragma pack(push, 1)
 
-			union {
-				U full_;
-				T t_[2];
-			} data_;
+            union {
+                U full_;
+                T t_[2];
+            } data_;
 
 #pragma pack(pop)
 
-			constexpr static std::size_t size() noexcept
-			{
-				return sizeof(U);
-			}
+            constexpr static std::size_t size() noexcept { return sizeof(U); }
 
-			constexpr part(T maj, T min)
-				: data_{ .t_ = { maj, min } }
-			{}
-			constexpr part(U full)
-				: data_{ .full_ = full }
-			{}
+            constexpr part(T maj, T min)
+                : data_{.t_ = {maj, min}} {}
+            constexpr part(U full)
+                : data_{.full_ = full} {}
 
-			constexpr T major() const
-			{
-				return data_.t_[0];
-			}
+            constexpr T major() const { return data_.t_[0]; }
 
-			constexpr T minor() const
-			{
-				return data_.t_[1];
-			}
+            constexpr T minor() const { return data_.t_[1]; }
 
-			constexpr U full() const
-			{
-				return data_.full_;
-			}
-		};
-	}
+            constexpr U full() const { return data_.full_; }
+        };
+    } // namespace details
 
-	class version : public details::part<std::uint16_t, std::uint32_t>
-	{
-		using base_t = details::part<std::uint16_t, std::uint32_t>;
-	public:
-		constexpr version(std::uint16_t maj, std::uint16_t min)
-			: base_t(maj, min)
-		{}
+    class version : public details::part<std::uint16_t, std::uint32_t> {
+        using base_t = details::part<std::uint16_t, std::uint32_t>;
 
-		constexpr version(std::uint32_t v)
-			: base_t(v)
-		{}
+      public:
+        constexpr version(std::uint16_t maj, std::uint16_t min)
+            : base_t(maj, min) {}
 
-		constexpr version(version const& v)
-			: base_t(v.major(), v.minor())
-		{}
+        constexpr version(std::uint32_t v)
+            : base_t(v) {}
 
-		constexpr version()
-			: base_t(0, 0)
-		{}
+        constexpr version(version const &v)
+            : base_t(v.major(), v.minor()) {}
 
-	};
+        constexpr version()
+            : base_t(0, 0) {}
+    };
 
-	class revision : public details::part<std::uint32_t, std::uint64_t>
-	{
-		using base_t = details::part<std::uint32_t, std::uint64_t>;
-	public:
-		constexpr revision(std::uint16_t a, std::uint16_t b, std::uint16_t c, std::uint16_t d)
-			: base_t(version(a, b).full(), version(c, d).full())
-		{}
+    class revision : public details::part<std::uint32_t, std::uint64_t> {
+        using base_t = details::part<std::uint32_t, std::uint64_t>;
 
-		constexpr revision(std::uint32_t maj, std::uint32_t min)
-			: base_t(maj, min)
-		{}
+      public:
+        /**
+         * @param a major
+         * @param b minor
+         * @param c patch
+         * @param d tag
+         */
+        constexpr revision(std::uint16_t a, std::uint16_t b, std::uint16_t c, std::uint16_t d)
+            : base_t(version(a, b).full(), version(c, d).full()) {}
 
-		constexpr revision(std::uint64_t v)
-			: base_t(v)
-		{}
+        constexpr revision(std::uint32_t maj, std::uint32_t min)
+            : base_t(maj, min) {}
 
-		constexpr revision(revision const& v)
-			: base_t(v.major(), v.minor())
-		{}
+        constexpr revision(std::uint64_t v)
+            : base_t(v) {}
 
-		constexpr revision()
-			: base_t(0, 0)
-		{}
+        constexpr revision(revision const &v)
+            : base_t(v.major(), v.minor()) {}
 
-	};
+        constexpr revision()
+            : base_t(0, 0) {}
+    };
 
-	// constexpr std::strong_ordering operator<=>(version lhs, version rhs)
-	// {
-	// 	return lhs.full() <=> rhs.full();
-	// }
+    // constexpr std::strong_ordering operator<=>(version lhs, version rhs)
+    // {
+    // 	return lhs.full() <=> rhs.full();
+    // }
 
-	// constexpr std::strong_ordering operator<=>(revision lhs, revision rhs)
-	// {
-	// 	return lhs.full() <=> rhs.full();
-	// }
+    // constexpr std::strong_ordering operator<=>(revision lhs, revision rhs)
+    // {
+    // 	return lhs.full() <=> rhs.full();
+    // }
 
-}
+} // namespace cyng
 
 #include <functional>
 
 namespace std {
 
-	template <>
-	class hash<cyng::version> {
-	public:
-		size_t operator()(cyng::version const&) const noexcept;
-	};
-	template <>
-	class hash<cyng::revision> {
-	public:
-		size_t operator()(cyng::revision const&) const noexcept;
-	};
+    template <> class hash<cyng::version> {
+      public:
+        size_t operator()(cyng::version const &) const noexcept;
+    };
+    template <> class hash<cyng::revision> {
+      public:
+        size_t operator()(cyng::revision const &) const noexcept;
+    };
 
-}
+} // namespace std
 
-#endif 
+#endif
--- include/cyng/parse/version.h
+++ include/cyng/parse/version.h
@@ -7,21 +7,19 @@
 #ifndef CYNG_PARSE_VERSION_H
 #define CYNG_PARSE_VERSION_H
 
-
 #include <cyng/obj/intrinsics/version.h>
 #include <string>
 
 namespace cyng {
 
-	/**
-	 * expected format is "m.m"
-	 */
-	version to_version(std::string const& str);
+    /**
+     * expected format is "m.m" (major, minor)
+     */
+    version to_version(std::string const &str);
 
-	/**
-	 * expected format is "m.m.p.t"
-	 */
-	revision to_revision(std::string const& str);
-}
+    /**
+     * expected format is "m.m.p.t" (major, minor, patch, tag)
+     */
+    revision to_revision(std::string const &str);
+} // namespace cyng
 #endif
-
